205c36e3d3bedd706749813f1153edcb4b3b64fc
Rupangkan <every1isnotrupangkan@gmail.com>
2022-01-04 22:40:16 +0530

    Added pdf viewing functionality in the app

--- app/src/main/java/com/repose/noted/PdfViewer.kt
+++ app/src/main/java/com/repose/noted/PdfViewer.kt
@@ -1,17 +1,20 @@
 package com.repose.noted
 
-import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
+import com.github.barteksc.pdfviewer.PDFView
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.repose.noted.databinding.ActivityPdfviewerBinding
 import com.repose.noted.model.AppViewModel
+
 
 private const val name = "question-papers"
 
@@ -39,11 +42,12 @@ private val COURSES = mapOf(
 
 class PdfViewer: AppCompatActivity() {
 
-    private lateinit var binding: ActivityPdfviewerBinding
+//    private lateinit var binding: ActivityPdfviewerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPdfviewerBinding.inflate(layoutInflater)
+//        binding = ActivityPdfviewerBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_pdfviewer)
 
 
         var bundle :Bundle ?= intent.extras
@@ -65,41 +69,48 @@ class PdfViewer: AppCompatActivity() {
         val semester = SEMESTER[semester]
         val storageRef =  FirebaseStorage.getInstance().reference
         var pdfRef = storageRef.child("$course/$semester/$name/$pdfName")
-        val temp = storageRef.child("$course/$semester/$name/$pdfName").toString()
+        val url = storageRef.child("$course/$semester/$name/$pdfName").toString()
 
-        Log.d("url", temp)
-        binding.progressBar.visibility = View.VISIBLE
-//        val ref = FirebaseStorage.getInstance().getReferenceFromUrl("gs://noted-app-b0c56.appspot.com/$course/$semester/$name/$pdfName")
+        val pdfView = findViewById<PDFView>(R.id.pdfView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        Log.d("url", url)
+//        binding.progressBar.visibility = View.VISIBLE
+        val ref = FirebaseStorage.getInstance().getReferenceFromUrl("gs://noted-app-b0c56.appspot.com/$course/$semester/$name/$pdfName")
         pdfRef.getBytes(50000000)
             .addOnSuccessListener { bytes ->
                 Log.d("Success", bytes.toString())
-                binding.pdfView.fromBytes(bytes)
-                    .onRender { nbPages, pageWidth, pageHeight ->
-                        binding.pdfView.fitToWidth()
-                    }
-                    .onDraw { canvas, pageWidth, pageHeight, displayedPage ->
-
-                    }
+                pdfView.fromBytes(bytes)
                     .swipeHorizontal(false)
 //                    .onPageChange{ page, pageCount ->
 //
 //                    }
                     .onError {
                         Log.d("Error", "Error")
+                        Toast.makeText(this, "Error loading pdf", Toast.LENGTH_SHORT)
                     }
 //                    .enableAnnotationRendering(true)
                     .onPageError { page, t ->
                         Log.d("Error", "Page error")
+                        Toast.makeText(this, "Error loading page", Toast.LENGTH_SHORT)
+
                     }
                     .load()
 
-                binding.progressBar.visibility = View.GONE
+                progressBar.visibility = View.GONE
 
             }
             .addOnFailureListener {
                 Log.d("Error", "Loading pdf failed")
-                binding.progressBar.visibility = View.GONE
+                progressBar.visibility = View.GONE
             }
+
+//        val webView = findViewById<WebView>(R.id.webView)
+//        webView.webViewClient = WebViewClient()
+//        webView.settings.setSupportZoom(true)
+//        webView.settings.javaScriptEnabled = true
+////        val url = FileUtils.getPdfUrl()
+//        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$url")
     }
 
     override fun onSupportNavigateUp(): Boolean {
